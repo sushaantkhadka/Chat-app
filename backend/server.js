3c1e5dcdc25efa83f3f31d2f08941a9cea6ecce9
Sushant <sushantkhadka2060@gmail.com>
2024-06-26 14:19:09 +0545

    routing and auth

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authroutes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("helloworld!!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB
+    console.log(`Server Running on Port:${PORT}`)
+});
