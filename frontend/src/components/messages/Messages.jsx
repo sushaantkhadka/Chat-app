b02ea28fcc7b9089e35e6570867ef56c08605ecc
Sushant <sushantkhadka2060@gmail.com>
2024-07-10 18:26:33 +0545

    search functioonnality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
 import useConversation from '../../store/useConversation'
@@ -6,12 +6,21 @@ import useConversation from '../../store/useConversation'
 export default function Messages() {
   const {messages, loading} = useGetMessages()
   const {selectedConversation} = useConversation();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behaviour: "smooth" });
+    },100 )
+  },[messages])
 
   console.log("messages:" ,messages);
   return (
     <div className='px-4 flex-1 overflow-auto'>
       {!loading && messages.length > 0 && messages.map((message) => (
-         <Message key={message._id} message={message} /> 
+         <div key={message._id} ref={lastMessageRef}>
+          <Message key={message._id} message={message} />
+         </div> 
       ))}
 
        {!loading && messages.length === 0 && (
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { styles } from "../../styles";
+import useConversation from '../../store/useConversation'
 import { IoSearchSharp } from "react-icons/io5";
+import useGetConversation from '../../hooks/useGetConversations'
+import toast from "react-hot-toast";
 
 export default function SearchInput() {
+  const [search, setSearch] = useState("");
+  const {setSelectedConversation} = useConversation();
+  const { conversations } = useGetConversation()
+
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    if(!search) return;
+    if(search.length < 3){
+      return toast.error('Search should be atleast 3 Character Long')
+    }
+
+    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLocaleLowerCase()))
+
+    if(conversation) {
+      setSelectedConversation(conversation)
+      setSearch('');
+    } else toast.error("No such user found")
+  }
   return (
     <>
-      <form className="flex items-start gap-2">
+      <form onSubmit={_handleSubmit} className="flex items-start gap-2">
         <input
           type="text"
           placeholder="Search"
           className={`${styles.primary} px-6 py-2 outline-none text-[#333333] `}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <button
           type="submit"
