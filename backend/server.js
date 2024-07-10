c8323216b49b1847069464678b8476cc0656aac4
Sushant <sushantkhadka2060@gmail.com>
2024-07-10 18:59:05 +0545

    socket basic setup

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -26,7 +26,7 @@ app.use("/api/users", userRoutes);
 //   res.send("helloworld!!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on Port:${PORT}`);
 });
