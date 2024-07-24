0c82177fdeaf4320cad6f70cb07ec3cf7d1a37eb
Sushant <sushantkhadka2060@gmail.com>
2024-07-24 16:52:42 +0545

    finalize

--- frontend/src/components/messages/SendMessage.jsx
+++ frontend/src/components/messages/SendMessage.jsx
@@ -19,11 +19,11 @@ export default function SendMessage() {
   return (
     <>
       <form onSubmit={_handleSubimt}>
-        <div className="w-full pl-3 pr-1 py-1 rounded-3xl border border-gray-200 items-center gap-2 inline-flex justify-between">
-          <div className="flex items-center gap-2">
+        <div className="w-full pl-3 pr-1 py-1 mb-1 rounded-3xl border border-gray-200 items-center gap-2 inline-flex justify-between">
+          <div className="flex items-center gap-2 w-full">
             <RxAvatar className="text-[#4daecb] " />
             <input
-              className="grow shrink basis-0 w-[100%] bg-[#f5f3eb] text-black text-xs font-medium leading-4 p-2 rounded-lg outline-none border-none placeholder-gray-300"
+              className="grow shrink basis-0 w-full bg-[#f5f3eb] text-black text-xs font-medium leading-4 p-2 rounded-lg outline-none  placeholder-gray-300"
               placeholder="Type here..."
               value={message}
               onChange={(e) => setMessage(e.target.value) }
